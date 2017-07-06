ea0095bb0daac817d6f73e9b8a6c446db5fda4f0
Virgilio Gomez-Rubio <Virgilio.Gomez@uclm.es>
2017-07-06 15:27:56 +0200

    Initial commit.

---
+++ R/sumR.R
@@ -0,0 +1,21 @@
+#' @name sumR
+#' @rdname sumR
+#'
+#' @title Addition of two numbers
+#'
+#' @description This package contains several functions to compute the addition
+#' of two numbers.
+#'
+#' @param x A number.
+#' @param y A number.
+#'
+#' @return The addition of \code{x} and \code{y}.
+#' 
+#' @examples
+#' sumR(1, 2)
+#'
+#' @export
+sumR <- function(x, y) {
+  return(x + y)
+}
+
